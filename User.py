c48de9488f236752b7feef9698219ce74dd571c6
Rodgersouko <gabrielaouko@gmail.com>
2020-10-10 08:27:08 +0300

    User class created

---
+++ User.py
@@ -0,0 +1,24 @@
+class User:
+
+    '''
+    A class that gets users input innstance 
+    '''
+
+    list = []
+    def__init__(self, name, password)
+        self.name = name
+        self.password = password
+
+
+self.name = 'rodgers'
+print('')
+
+
+    # def save_details(self):
+    #     '''
+    #     method for saving object to the users list
+    #     '''
+    # User.list.append(self)
+    
+    # self.name = ('rodgers','come')
+    # print(name)
