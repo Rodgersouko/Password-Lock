5f7b6f48617db61ce4619e7cedd90a87d1ce774c
Rodgersouko <gabrielaouko@gmail.com>
2020-10-11 20:02:42 +0300

    created functions for saving and deleting objects in the list

--- User.py
+++ User.py
@@ -11,25 +11,54 @@ class User:
         self.name = name
         self.password = password
 
+
+
+    def save_user(self):
+
+        '''
+        function for saving user objects to list
+        '''
+
+        User.list.append(self)
+
+
+    def delete_user(self):
+
+        '''
+        function for deleting objects from list
+        '''
+
+        User.list.remove(self)    
+
+    @classmethod
+    def display_accounts(cls):
+        '''
+        function that returns the list
+        '''
+        return cls.list    
+
+        
     print("Welcome to Password LocK. select one of the below options to continue:")
     print("\n1. Login \n2. Register new account \n3. Exit")
     user_option = input()
 
     if user_option == "1":
-        print("Login")
+            print("Create a Password Locke Account")
+            print ('\n')
+            account_name = input('Account name:')
+            print ('\n')
+            user_name = input('User name:')
+            print ('\n')
     elif user_option == "2":
-        create_account()
+            user_name = input('New User Name : ')
+            # print('/n')
+            pwd = input('Password : ')
+            print ('\n')
+
     elif user_option == "3":
-        exit()
+            print(f"Have a nice day...")
+            # break
     else:
         print("Invalid option")
 
-
-
-
-
-
-
-
-
- 
+    
